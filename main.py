9348beb7fa0635fddf448577029c87e6ba8ab6da
sashenika <sashenika.k@mail.ru>
2022-04-24 13:41:12 +0300

    heroku

--- main.py
+++ main.py
@@ -178,7 +178,8 @@ def galery():
 
 def main():
     db_session.global_init("db/turizm.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
